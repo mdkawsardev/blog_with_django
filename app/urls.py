00e16eb77e6e0d6cebb675580fb88c99f41ad1b5
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-19 15:56:48 +0600

    Blogging site is being developed

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='home'),
+    path('blog/', views.blog, name='blog'),
+    path('post/', views.post, name='post'),
+    path('details/', views.details, name='details'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def post(request):
+    return render(request, 'post.html')
+
+def details(request):
+    return render(request, 'details.html')
