1190b0be0218e791cf63c85d34f6f150f1fe0a47
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-20 18:37:56 +0600

    backend is going on

--- app/models.py
+++ app/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category = models.CharField(max_length=255)
-    
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     banner = models.ImageField(upload_to='media', blank=True, default='project1.png')
     description = models.TextField()
+    select = models.ForeignKey(Category, on_delete=models.CASCADE)
 
