ffaaab9ecd27ead4a26307288a85fa27b9142434
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-20 19:08:06 +0600

    backend is going on

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Category, Select, Post, Number
 # Register your models here.
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category']
 admin.site.register(Select)
 admin.site.register(Post)
 admin.site.register(Number)
