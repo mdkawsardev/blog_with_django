8d932233fe60aae2874d74f60855a88e253c7611
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-20 18:40:33 +0600

    backend is going on

--- app/models.py
+++ app/models.py
@@ -10,4 +10,5 @@ class Post(models.Model):
     banner = models.ImageField(upload_to='media', blank=True, default='project1.png')
     description = models.TextField()
 
-
+class Select(models.Model):
+    select = models.ForeignKey(Category, on_delete=models.CASCADE)
