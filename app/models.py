cda08ef9d441a4b7f3112efaec021ca98abacfc8
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-20 18:34:20 +0600

    backend is going on

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+    
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     banner = models.ImageField(upload_to='media', blank=True, default='project1.png')
     description = models.TextField()
+
