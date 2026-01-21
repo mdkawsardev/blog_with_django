604ab1c2bc35421ee47b2d4310bd3112427e2bdf
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-21 12:17:25 +0600

    backend is going on

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    banner = models.ImageField(upload_to='media', blank=True, default='project1.png')
+    banner = models.ImageField(upload_to='media/', blank=True, default='project1.png')
     description = models.TextField()
 
 class Select(models.Model):
