55f4342ff3e83bc849e9865b6bb3f28c722a0ce5
mdkawsar <info.mdkawsardev@gmail.com>
2026-02-18 20:46:12 +0600

    Site is being ready for deployment

--- project/settings.py
+++ project/settings.py
@@ -73,16 +73,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "blog_site",
-#         "USER": "blogadmin",
-#         "PASSWORD": "blogadmin123",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog_site",
+        "USER": "blogadmin",
+        "PASSWORD": "blogadmin123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
